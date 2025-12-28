9c3efc531a5e4dceaf86846ebe0b286d955bd4de
Amir <amirmohammadyosefi290@gmail.com>
2025-12-28 19:00:46 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests , os , psutil , sys , jwt , pickle , json , binascii , time , urllib3 , base64 , datetime , re , socket , threading , ssl , pytz , aiohttp
 import asyncio
 from flask import Flask, request, jsonify
-from protobuf_decoder.protobuf_decoder import Parser
+# از protobuf_decoder خط زیر رو حذف کردیم
+# from protobuf_decoder.protobuf_decoder import Parser
 from xC4 import * ; from xHeaders import *
 from datetime import datetime
 from google.protobuf.timestamp_pb2 import Timestamp
@@ -34,8 +35,8 @@ Hr = {
     'Content-Type': "application/x-www-form-urlencoded",
     'Expect': "100-continue",
     'X-Unity-Version': "2018.4.11f1",
-    'X-GA': "v1 1",
-    'ReleaseVersion': "OB51"}
+    'X-GA": "v1 1",
+    'ReleaseVersion": "OB51"}
 
 # ---- تابع encrypted_proto که در xC4.py هست ----
 async def encrypted_proto(encoded_hex):
@@ -89,8 +90,6 @@ def get_jwt_from_github():
     return None, None
 
 # ---- توابع اصلی از xC4.py ----
-# این توابع از xC4 import شده‌اند، اما اگر مشکل داشتیم اینجا هم تعریف می‌کنیم
-
 async def GeNeRaTeAccEss(uid , password):
     """دریافت توکن از گارنا - برای backward compatibility"""
     url = "https://100067.connect.garena.com/oauth/guest/token/grant"
@@ -124,62 +123,23 @@ async def EncRypTMajoRLoGin(open_id, access_token):
     major_login.client_version = "1.118.1"
     major_login.system_software = "Android OS 9 / API-28 (PQ3B.190801.10101846/G9650ZHU2ARC6)"
     major_login.system_hardware = "Handheld"
-    major_login.telecom_operator = "Verizon"
-    major_login.network_type = "WIFI"
-    major_login.screen_width = 1920
-    major_login.screen_height = 1080
-    major_login.screen_dpi = "280"
-    major_login.processor_details = "ARM64 FP ASIMD AES VMH | 2865 | 4"
-    major_login.memory = 3003
-    major_login.gpu_renderer = "Adreno (TM) 640"
-    major_login.gpu_version = "OpenGL ES 3.1 v1.46"
     major_login.unique_device_id = "Google|34a7dcdf-a7d5-4cb6-8d7e-3b0e448a0c57"
-    major_login.client_ip = "223.191.51.89"
     major_login.language = "en"
+    
+    # استفاده از JWT آماده
+    major_login.access_token = access_token
+    
+    # تنظیم open_id و open_id_type بر اساس account_uid
     major_login.open_id = open_id
     major_login.open_id_type = "4"
-    major_login.device_type = "Handheld"
     
-    # اضافه کردن memory_available
-    from Pb2.MajoRLoGinrEq_pb2 import GameSecurity
-    memory_available = GameSecurity()
-    memory_available.version = 55
-    memory_available.hidden_value = 81
-    major_login.memory_available.CopyFrom(memory_available)
-    
-    major_login.access_token = access_token
+    # سایر تنظیمات
     major_login.platform_sdk_id = 1
-    major_login.network_operator_a = "Verizon"
-    major_login.network_type_a = "WIFI"
-    major_login.client_using_version = "7428b253defc164018c604a1ebbfebdf"
-    major_login.external_storage_total = 36235
-    major_login.external_storage_available = 31335
-    major_login.internal_storage_total = 2519
-    major_login.internal_storage_available = 703
-    major_login.game_disk_storage_available = 25010
-    major_login.game_disk_storage_total = 26628
-    major_login.external_sdcard_avail_storage = 32992
-    major_login.external_sdcard_total_storage = 36235
     major_login.login_by = 3
-    major_login.library_path = "/data/app/com.dts.freefireth-YPKM8jHEwAJlhpmhDhv5MQ==/lib/arm64"
-    major_login.reg_avatar = 1
-    major_login.library_token = "5b892aaabd688e571f688053118a162b|/data/app/com.dts.freefireth-YPKM8jHEwAJlhpmhDhv5MQ==/base.apk"
     major_login.channel_type = 3
-    major_login.cpu_type = 2
-    major_login.cpu_architecture = "64"
-    major_login.client_version_code = "2019118695"
-    major_login.graphics_api = "OpenGLES2"
-    major_login.supported_astc_bitset = 16383
     major_login.login_open_id_type = 4
-    major_login.analytics_detail = b"FwQVTgUPX1UaUllDDwcWCRBpWAUOUgsvA1snWlBaO1kFYg=="
-    major_login.loading_time = 13564
     major_login.release_channel = "android"
-    major_login.extra_info = "KqsHTymw5/5GB23YGniUYN2/q47GATrq7eFeRatf0NkwLKEMQ0PK5BKEk72dPflAxUlEBir6Vtey83XqF593qsl8hwY="
-    major_login.android_engine_init_flag = 110009
-    major_login.if_push = 1
-    major_login.is_vpn = 1
-    major_login.origin_platform_type = "4"
-    major_login.primary_platform_type = "4"
+    
     string = major_login.SerializeToString()
     return await encrypted_proto(string)
 
@@ -270,13 +230,6 @@ async def quick_session_with_jwt(team_code: str, uids: list, emote_id: int, jwt_
         # تنظیم open_id و open_id_type بر اساس account_uid
         major_login.open_id = str(account_uid)
         major_login.open_id_type = "4"
-        
-        # اضافه کردن memory_available
-        from Pb2.MajoRLoGinrEq_pb2 import GameSecurity
-        memory_available = GameSecurity()
-        memory_available.version = 55
-        memory_available.hidden_value = 81
-        major_login.memory_available.CopyFrom(memory_available)
         
         # سایر تنظیمات
         major_login.platform_sdk_id = 1
